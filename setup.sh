ea5ed943d992237c9698ee0f0988949fb310fda1
Celso Mireles <celso.mireles@gmail.com>
2017-07-07 23:31:37 -0700

    Builds setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
 echo "Setting up Redadalertas Docker Stack..."
+
+read -p "Enter the url of your 'redadalertas' clone: " CLIENT_URL
+
+read -p "Enter the url of your 'redadalertas-api' clone: " SERVER_URL
+
+echo "Client Url: $CLIENT_URL"
+
+echo "Server Url: $SERVER_URL"
+
+echo "Removing client and server directories..."
+
+rm -rf redadalertas 
+rm -rf redadalertas-api 
+
+echo "Now making the directories again..."
+
+mkdir redadalertas
+mkdir redadalertas-api
+
+echo "Cloning the repos"
+
+git clone $CLIENT_URL redadalertas
+git clone $SERVER_URL redadalertas-api
